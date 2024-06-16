692b2be3f5c543288fc7d28e69aa9ee6298f66bc
Choi JongHyeon <gosury32@dgist.ac.kr>
2024-06-16 17:29:04 +0900

    2

--- hw2.c
+++ hw2.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "bmplib.h"
 #include "hw2.h"
 
 // Optimized convolution function
-static Pixel convolution(
-        Pixel* input, int x, int y, int width, int height, float* filter) {
+static Pixel convolution(Pixel* input, int x, int y, int width, int height, float* filter, int* filterIndices) {
     double r = 0;
     double g = 0;
     double b = 0;
 
-    int filterSize = 3;
-    int halfFilterSize = filterSize / 2;
-    int filterIndex = 0;
+    for (int i = 0; i < 9; ++i) {
+        int dx = filterIndices[2 * i];
+        int dy = filterIndices[2 * i + 1];
 
-    for (int dy = -halfFilterSize; dy <= halfFilterSize; ++dy) {
-        for (int dx = -halfFilterSize; dx <= halfFilterSize; ++dx) {
-            int ix = x + dx;
-            int iy = y + dy;
+        int ix = x + dx;
+        int iy = y + dy;
 
-            if (ix >= 0 && ix < width && iy >= 0 && iy < height) {
-                Pixel* pixel = &input[ix + iy * width];
-                float f = filter[filterIndex];
+        if (ix >= 0 && ix < width && iy >= 0 && iy < height) {
+            Pixel* pixel = &input[ix + iy * width];
+            float f = filter[i];
 
-                r += pixel->r * f;
-                g += pixel->g * f;
-                b += pixel->b * f;
-            }
-            filterIndex++;
+            r += pixel->r * f;
+            g += pixel->g * f;
+            b += pixel->b * f;
         }
     }
 
@@ -48,10 +42,23 @@ void filter_optimized(void* args[]) {
     Pixel* output = (Pixel*)args[3];
     float* filter = (float*)args[4];
 
+    int filterIndices[18];
+    int filterSize = 3;
+    int halfFilterSize = filterSize / 2;
+
+    // Precompute filter indices
+    int index = 0;
+    for (int dy = -halfFilterSize; dy <= halfFilterSize; ++dy) {
+        for (int dx = -halfFilterSize; dx <= halfFilterSize; ++dx) {
+            filterIndices[2 * index] = dx;
+            filterIndices[2 * index + 1] = dy;
+            index++;
+        }
+    }
+
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            output[x + y * width] = convolution(input, x, y, width, height, filter);
+            output[x + y * width] = convolution(input, x, y, width, height, filter, filterIndices);
         }
     }
 }
-
